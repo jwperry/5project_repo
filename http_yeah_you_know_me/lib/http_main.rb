da97d62d3b1d19d7b10b3adc23d986df2cbf5c0f
Steven Olson <steveoscaro@gmail.com>
2015-11-02 17:13:13 -0700

    finished iteration 2

--- http_yeah_you_know_me/lib/http_main.rb
+++ http_yeah_you_know_me/lib/http_main.rb
@@ -4,15 +4,35 @@ require './response_parser'
 
 parser = ResponseParser.new
 tcp_server = TCPServer.new(9292)
-count = 0
 
-client = tcp_server.accept
-puts "Ready for a request"
-request_lines = []
-while line = client.gets and !line.chomp.empty?
-  request_lines << line.chomp
+counter = 0
+hello_counter = 0
+
+loop do
+  counter += 1
+  client = tcp_server.accept
+  puts "Ready for a request"
+  request_lines = []
+  while line = client.gets and !line.chomp.empty?
+    request_lines << line.chomp
+  end
+  puts "Got this request:"
+  puts request_lines.inspect
+  case parser.get_path(request_lines)
+  when "/hello"
+    parser.hello_response(client, hello_counter)
+    hello_counter += 1
+  when "/datetime"
+    parser.datetime_response(client)
+  when "/shutdown"
+    parser.shutdown_response(counter, client)
+    client.close
+    break
+  else
+    parser.default_response(request_lines, client)
+  end
+
 end
-puts "Got this request:"
-puts request_lines.inspect
 
-parser.default_response(request_lines, client)
+# parser.default_response(request_lines, client)
+# /hello /datetime /shutdown
--- http_yeah_you_know_me/lib/response_parser.rb
+++ http_yeah_you_know_me/lib/response_parser.rb
@@ -1,8 +1,6 @@
 require 'pry'
+require 'date'
 class ResponseParser
-
-  def initialize
-  end
 
   def get_verb(string)
     string[0].split(" ")[0]
@@ -55,4 +53,29 @@ class ResponseParser
     puts "\nResponse complete, exiting."
   end
 
+  def hello_response(client, hello_counter)
+    response = "<pre>" + "Hello world! (#{hello_counter})" + "</pre>"
+    output = "<html><head></head><body>#{response}</body></html>"
+    headers =
+    client.puts headers
+    client.puts output
+  end
+
+  def datetime_response(client)
+    time = DateTime.now
+    time_response = time.strftime('%l:%M%P on %A, %B %-d, %Y')
+    response = "<pre>" + "#{time_response}" + "</pre>"
+    output = "<html><head></head><body>#{response}</body></html>"
+    headers =
+    client.puts headers
+    client.puts output
+  end
+
+  def shutdown_response(counter, client)
+    response = "<pre>" + "Total Requests: (#{counter})" + "</pre>"
+    output = "<html><head></head><body>#{response}</body></html>"
+    headers =
+    client.puts headers
+    client.puts output
+  end
 end
