293f898615d39b71a4c0468c7729eb9b5bb7b17b
Joe Perry <joseph.w.perry@gmail.com>
2015-11-04 18:20:16 -0700

    Server class refactoring

--- http_yeah_you_know_me/lib/http_main.rb
+++ http_yeah_you_know_me/lib/http_main.rb
@@ -1,17 +1,11 @@
 require 'pry'
 require 'socket'
-require_relative 'response_parser'
-require_relative 'parser'
 require_relative 'game'
-
-parser = Parser.new
-response_parser = ResponseParser.new
+require_relative 'server'
 tcp_server = TCPServer.new(9292)
 game = Game.new
-
+server = Server.new
 counter = 0
-hello_counter = 0
-guess = 0
 
 loop do
   counter += 1
@@ -22,49 +16,22 @@ loop do
   while line = client.gets and !line.chomp.empty?
     request_lines << line.chomp
   end
-  verb = parser.get_verb(request_lines)
-  path = parser.get_path(request_lines)
 
-  #client.puts "<pre> Good Luck! </pre>" if game.start_game?(request_lines, path) && game.guess_counter == 0
-  if verb == "POST" && path.start_with?("/start_game") && game.guess_counter > 0
-    client.puts "HTTP/1.1 403 Forbidden\r\n\r\n#{"403 Forbidden\nGame in progress!"}"
-  elsif game.start_game?(request_lines, path)
-    client.puts "<pre> Good Luck! Use /start_game after guessing correctly to restart! </pre>"
-  elsif path.start_with?("/game") && verb == "POST"
-    guess = parser.get_guess(path)
-    client.puts "HTTP/1.1 302 Found\r\nLocation: http://127.0.0.1:9292/game"
-  elsif path.start_with?("/game")
-    answer = game.answer
-    output = game.check_guess(guess, answer)
-    client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
-  elsif path.start_with?("/word_search")
-    word = parser.get_word(path)
-    output = response_parser.word_search_response(word)
-    client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
-  elsif path == "/hello"
-    hello_counter += 1
-    output = response_parser.hello_response(hello_counter)
-    client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
-  elsif path == "/datetime"
-    output = response_parser.datetime_response
-    client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
-  elsif path == "/shutdown"
-    output = response_parser.shutdown_response(counter)
-    client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
-    client.close
-    break
-  elsif path == "/"
-    output = response_parser.default_response(request_lines)
-    client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
-  elsif path == "/force_error"
-    client.puts "HTTP/1.1 500 INTERNAL SERVER ERROR \r\n\r\n500 Internal Server Error"
-    raise 'SystemError'
-  else
-    client.puts "HTTP/1.1 404 NOT FOUND\r\n\r\n404 NOT FOUND"
+  server.update_request_lines(request_lines)
+  break if server.shutdown(client, counter)
+  if server.game_in_progress(game.guess_counter, client)
+  elsif server.start_game(client)
+  elsif server.redirect(client)
+  elsif server.check_guess(client)
+  elsif server.word_search(client)
+  elsif server.hello(client)
+  elsif server.date_time(client)
+  elsif server.default_response(client)
+  elsif server.force_error(client)
+  else not_found(client)
   end
-
+  
   puts "Got this request:"
   puts request_lines.inspect
   client.close
-
 end
---
+++ http_yeah_you_know_me/lib/server.rb
@@ -0,0 +1,114 @@
+require 'pry'
+require_relative 'game'
+require_relative 'response_parser'
+require_relative 'parser'
+
+class Server
+  attr_accessor :request_lines, :hello_counter
+
+  def initialize
+    @parser = Parser.new
+    @game = Game.new
+    @response_parser = ResponseParser.new
+    @guess = ""
+    @hello_counter = 0
+  end
+
+  def get_verb
+    @parser.get_verb(@request_lines)
+  end
+
+  def get_path
+    @parser.get_path(@request_lines)
+  end
+
+  def update_request_lines(request_lines)
+    @request_lines = request_lines
+  end
+
+  def game_in_progress(guess_counter, client)
+    if get_verb == "POST" && get_path.start_with?("/start_game") && @game.guess_counter > 0
+      client.puts "HTTP/1.1 403 Forbidden\r\n\r\n#{"403 Forbidden\nGame in progress!"}"
+      return get_verb == "POST" && get_path.start_with?("/start_game") && @game.guess_counter > 0
+    end
+  end
+
+  def start_game(client)
+    if @game.start_game?(@request_lines, get_path)
+      client.puts "<pre> Good Luck! Use /start_game after guessing correctly to restart! </pre>"
+      return @game.start_game?(@request_lines, get_path)
+    end
+  end
+
+  def redirect(client)
+    if get_path.start_with?("/game") && get_verb == "POST"
+      @guess = @parser.get_guess(get_path)
+      client.puts "HTTP/1.1 302 Found\r\nLocation: http://127.0.0.1:9292/game"
+      return get_path.start_with?("/game") && get_verb == "POST"
+    end
+  end
+
+  def check_guess(client)
+    if get_path.start_with?("/game")
+      answer = @game.answer
+      output = @game.check_guess(@guess, answer)
+      client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
+      return get_path.start_with?("/game")
+    end
+  end
+
+  def word_search(client)
+    if get_path.start_with?("/word_search")
+      word = @parser.get_word(get_path)
+      output = @response_parser.word_search_response(word)
+      client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
+      return get_path.start_with?("/word_search")
+    end
+  end
+
+  def hello(client)
+    if get_path == "/hello"
+      @hello_counter += 1
+      output = @response_parser.hello_response(@hello_counter)
+      client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
+      return get_path == "/hello"
+    end
+  end
+
+  def date_time(client)
+    if get_path == "/datetime"
+      output = @response_parser.datetime_response
+      client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
+      return get_path == "/datetime"
+    end
+  end
+
+  def shutdown(client, counter)
+    if get_path == "/shutdown"
+      output = @response_parser.shutdown_response(counter)
+      client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
+      client.close
+      return get_path == "/shutdown"
+    end
+  end
+
+  def default_response(client)
+    if get_path == "/"
+      output = @response_parser.default_response(@request_lines)
+      client.puts "HTTP/1.1 200 OK\r\n\r\n#{output}"
+      return get_path == "/"
+    end
+  end
+
+  def force_error(client)
+    if get_path == "/force_error"
+      client.puts "HTTP/1.1 500 INTERNAL SERVER ERROR \r\n\r\n500 Internal Server Error"
+      return get_path == "/force_error"
+    end
+  end
+
+  def not_found(client)
+    client.puts "HTTP/1.1 404 NOT FOUND\r\n\r\n404 NOT FOUND"
+  end
+
+end
